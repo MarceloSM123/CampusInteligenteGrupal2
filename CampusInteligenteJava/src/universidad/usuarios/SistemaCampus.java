f022519e7725cd6df0a49e7c2a9bff38bd74fde9
kerveros12v <kerveros12v@aol.com>
2026-03-18 20:41:22 -0500

    Implementacion de funcion main

--- CampusInteligenteJava/src/universidad/usuarios/SistemaCampus.java
+++ CampusInteligenteJava/src/universidad/usuarios/SistemaCampus.java
@@ -1,5 +1,15 @@
 package universidad.usuarios;
-
+import universidad.usuarios.Estudiante;​
+import universidad.infraestructura.Laboratorio;
 public class SistemaCampus {
-
+public static void main(String[] args) {
+	Estudiante estudiante1=new Estudiante("","","",1);
+	Estudiante estudiante2=new Estudiante("","","",2);
+	Laboratorio laboratorio1=new Laboratorio("",5);
+	laboratorio1.mostrarInformacion();
+	laboratorio1.abrirLaboratorio();
+	estudiante1.presentarse();
+	estudiante1.estudiar();
+	estudiante2.presentarse();
+}
 }
